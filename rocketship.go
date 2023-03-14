d4faad5e4c62b43efe81e05c2c8d2d616f7d5d00
chownelson <nelsonc1506@gmail.com>
2023-03-14 13:12:04 -0400

    added variable to keep track of current planet

--- rocketship.go
+++ rocketship.go
@@ -26,7 +26,8 @@ func calculateFuel(planet string) int{
 
 // Create the function greetPlanet() here
 func greetPlanet(planet string) {
-  fmt.Println("Welcome to %v !", planet)
+  fmt.Printf("Welcome to %v!!", planet)
+  fmt.Println(" ")
 }
 
 // Create the function cantFly() here
@@ -35,33 +36,60 @@ func cantFly(){
 }
 
 // Create the function flyToPlanet() here
-func flyToPlanet(planet string, fuel int) int{
+func flyToPlanet(current string, planet string, fuel int) (string, int){
   var fuelRemaining int = fuel
   var fuelCost int = calculateFuel(planet)
+  var currentPlanet string = current
   if fuelRemaining > fuelCost{
     greetPlanet(planet)
     fuelRemaining = fuelRemaining - fuelCost
+    currentPlanet = planet
   } else{
     cantFly()
   }
-  return fuelRemaining
+  return currentPlanet, fuelRemaining
+}
+func options() {
+  defer fmt.Println("Type one of the shown options to travel.")
+  fmt.Println("Welcome to Planet Express. Your options are: ")
+  fmt.Println("Venus")
+  fmt.Println("Mercury")
+  fmt.Println("Mars")
 }
 
 func main() {
+  /*
   // Test your functions!
   var fuel int = 69
-  //fuelGauge(fuel)
-  // Create `planetChoice` and `fuel`
+  fuelGauge(fuel)
+
   var planetChoice string = "Mars"
-  //fuel = calculateFuel(planetChoice)
-  //fmt.Println(fuel)
-  // And then liftoff!
+  fuel = calculateFuel(planetChoice)
+  fmt.Println(fuel)
+
   fuel = 100000
   planetChoice = "Venus"
   fuel = flyToPlanet(planetChoice, fuel)
   fuelGauge(fuel)
+  */
 
-  //keep track of planet
-  //function to return spaceship back to home planet
-  //add more destinations and allow transportation between different planets
+  /*
+  keep track of planet
+  function to return spaceship back to home planet
+  add more destinations and allow transportation between different planets
+  add ToLower on function args?
+
+  */
+
+  var fuel int=1000000
+  var planetChoice string
+  options()
+  fmt.Scan(&planetChoice)
+  fuelGauge(fuel)
+  var currentPlanet string = "Earth"
+  currentPlanet, fuel = flyToPlanet(currentPlanet, planetChoice, fuel)
+  fuelGauge(fuel)
+  fmt.Println(currentPlanet)
+
+
 }
